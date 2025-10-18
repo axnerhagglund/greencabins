35bf6f1792a28927520bc955dcea6ce125ab1a20
axnerhagglund <axnerhagglund@gmail.com>
2025-10-18 17:33:54 +0200

    hero rdy, made button component

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Button from './Button'
 function Hero() {
   return (
-    <div className='bg-[url(/08-night.jpg)] h-screen bg-cover bg-center '></div>
+    <div className='bg-[url(/08-night.jpg)] h-screen bg-cover bg-center flex flex-col items-center justify-center '>
+      
+        <h1 className='text-white text-4xl md:text-6xl font-bold mb-4'>Welcome to nature</h1>
+        <p className='text-white text-lg md:text-2xl mb-8 max-w-2xl text-center'>Book the perfect <span className='border-b'>get away</span> cabin <br/> right now!</p>
+        <Button title="Explore Cabins" href="/cabins" buttonType="primary" />
+      
+    </div>
   )
 }
 
