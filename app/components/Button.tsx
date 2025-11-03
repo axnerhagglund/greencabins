72838a398c41e37172d724a8933dd1f8576d82b2
axnerhagglund <axnerhagglund@gmail.com>
2025-11-03 09:29:02 +0100

    fixed explore button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+"use client"
+import React, { FormEvent } from 'react'
 import Link from 'next/link'
 import { cn } from 'clsx-for-tailwind'
 
 type buttonProps = {
     title: string,
-    href: string
+    href: any
     buttonType: string
 }
 function Button({title, href, buttonType}: buttonProps) {
+   const handleClick = (e:FormEvent) => {
+    e.preventDefault();
+
+    if (href === "100vh") {
+      window.scrollBy({
+        top: window.innerHeight * 1, // 50% av viewport-höjd
+        behavior: "smooth",
+      });
+    } else if (href.startsWith("#")) {
+      const el = document.querySelector(href);
+      if (el) el.scrollIntoView({ behavior: "smooth" });
+    } else {
+      window.location.href = href; // fallback om det är en riktig länk
+    }
+  };
   return (
     <Link href={href}>
         <button className={cn(
@@ -16,7 +32,9 @@ function Button({title, href, buttonType}: buttonProps) {
             {"border-2 text-white px-6 py-3 rounded-full text-lg hover:bg-gray-500 transition": buttonType === "primary"},
             {"bg-gray-200 hover:bg-gray-300 text-black": buttonType === "secondary"},
             
-        )}>
+        )}
+        onClick={handleClick}>
+          
             {title}
         </button>
     </Link>
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,7 +6,7 @@ function Hero() {
       
         <h1 className='text-white text-4xl md:text-6xl font-bold mb-4'>Welcome to nature</h1>
         <p className='text-white text-lg md:text-2xl mb-8 max-w-2xl text-center'>Book the perfect <span className=' font-medium border-b'>get away</span> cabin <br/> right now!</p>
-        <Button title="Explore Cabins" href="/cabins" buttonType="primary" />
+        <Button title="Explore Cabins" href="100vh" buttonType="primary" />
       
     </div>
   )
